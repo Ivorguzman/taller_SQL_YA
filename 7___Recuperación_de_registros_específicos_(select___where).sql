453c17d1503eeb1f67cfa3724af98f24636dfc7e
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-11-23 23:58:19 -0400

    respaldo 23112022

---
+++ 7___Recuperación_de_registros_específicos_(select___where).sql
@@ -0,0 +1,24 @@
+/*
+Problema:
+Trabajamos con nuestra tabla "agenda".
+1- Eliminamos "agenda", si existe:
+drop table if exists agenda;
+2- Creamos la tabla, con los siguientes campos:
+nombre (cadena de 20), domicilio (cadena de 30) y telefono (cadena de 11).
+3- Visualice la estructura de la tabla "agenda".(describe).
+4- Ingrese los siguientes registros:
+'Alberto Mores','Colon 123','4234567',
+'Juan Torres','Avellaneda 135','4458787',
+'Mariana Lopez','Urquiza 333','4545454',
+'Fernando Lopez','Urquiza 333','4545454'.
+5- Seleccione todos los registros de la tabla. (select).
+6- Seleccione el registro cuyo nombre sea 'Juan Torres'.
+7- Seleccione el registro cuyo domicilio sea 'Colon 123'.
+8- Muestre los datos de quienes tengan el teléfono '4545454'.
+9- Elimine la tabla "agenda"
+
+*/ 
+
+
+DROP TABLE IF EXISTS agenda;
+
